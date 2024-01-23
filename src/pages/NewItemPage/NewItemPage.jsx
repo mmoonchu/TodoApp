08e197e38f767eb278aa6920dbab60581b93fd61
Moon Chu <mmoonchu2@gmail.com>
2024-01-23 08:20:53 -0500

    Resized NewItemPage text

--- src/pages/NewItemPage/NewItemPage.jsx
+++ src/pages/NewItemPage/NewItemPage.jsx
@@ -4,7 +4,7 @@ function NewItemPage() {
   return (
     <div>
       <h1>Create a To-do Item</h1>
-      <form action="/todo" method="POST"><br/>
+      <form className='form' action="/todo" method="POST"><br/>
         Title: <input style={{margin:"1em"}} type="text" name="title" /><br/>
         Description: <input  style={{margin:"1em"}} type="text" name="desc" /><br/>
         <input style={{margin:"1em"}} type="submit" value="Create Item" />
